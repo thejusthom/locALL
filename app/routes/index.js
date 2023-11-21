7a3d59eabd47984f21dc9182d5b52fd8d9ce6b46
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-11-20 20:24:56 -0500

    [REST APIs] including location and happening route in app routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import userRouter from './user-route.js'
+import locationRouter from './location-route.js';
+import happeningRouter from './happenings-route.js'
+
+const LocationEndpoint = '/location/:locationId';
 export default (app) => {
     app.use('/users',userRouter)
+    app.use('/location',locationRouter)
+    app.use(`${LocationEndpoint}/happenings`,happeningRouter)
 }
