e2fbdb50c7bb20de1bd3988c4f85fc9dd90017fc
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-11-20 20:23:33 -0500

    [REST APIs] happenings schema

---
+++ app/models/happenings.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const HappeningSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    postedDate: {
+        type: Date,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    locationId: {
+        type: String,
+        required: true
+    }
+},
+    {
+        versionKey: false
+    });
+
+const HappeningModel = mongoose.model('Happening', HappeningSchema);
+export default HappeningModel;
