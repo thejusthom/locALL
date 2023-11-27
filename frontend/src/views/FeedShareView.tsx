ad5a5d572a1d16c29e93f40e773308aa19821478
Thejus Thomson <thomson.th@northeastern.edu>
2023-11-27 14:09:07 -0500

    [ReactUI] - Adding sass dependencies

--- frontend/src/views/FeedShareView.tsx
+++ frontend/src/views/FeedShareView.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import food from '../assests/images/food1.jpg';
+import '../assests/styles/feedshare.scss';
 
 interface FeedShareProps {
   image: string;
@@ -6,19 +8,18 @@ interface FeedShareProps {
   address: string;
   servings: number;
   submitterDetails: string;
-  timePosted: string;
+  timePosted: Date;
 }
 
 const FeedShare = () => {
-  const image = '';
   const foodType = '';
-  const address = '';
+  const address = '71 Walnut Park';
   const servings = 5;
-  const submitterDetails = '';
-  const timePosted = '';  
+  const submitterDetails = 'Thejus Thomson';
+  const timePosted = '2023-11-26';  
   return (
     <div className="card">
-      <img src={image} alt="Food" className="card-image" />
+      <img src={food} alt="Food" className="card-image" />
       <div className="card-details">
         <h3>{foodType}</h3>
         <p>Address: {address}</p>
