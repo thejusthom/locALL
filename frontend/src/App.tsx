1dd9e31a4ee8f85d6414db7c571e643769218ada
Thejus Thomson <thomson.th@northeastern.edu>
2023-11-27 18:05:34 -0500

    [ReactUI] - Adding MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css'
 import EventsView from './views/EventsView';
 import FeedShare from './views/FeedShareView';
+import { StyledEngineProvider } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <div>kldj</div>
-      <EventsView />
-      <FeedShare/>
-          </div>
+    <StyledEngineProvider injectFirst>
+      <div className="App">
+        <div>kldj</div>
+        <EventsView />
+        <FeedShare/>
+      </div>
+    </StyledEngineProvider>
   );
 }
 
