b817a4ad70ee2af061dbc5e611862e89574d78f6
Pranav Prakash <prakash.pran@northeastern.edu>
2023-11-27 19:44:48 -0500

    [ReactStaticUI] - Added Routes in app,tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import './App.css'
 import EventsView from './views/EventsView';
 import FeedShare from './views/FeedShareView';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <div>kldj</div>
-      <EventsView />
-      <FeedShare/>
-          </div>
+    <Routes />
   );
 }
 
