4df1c8c3b786e7f76379b7efc1939f6a92b9c714
Pranav Prakash <prakash.pran@northeastern.edu>
2023-12-03 01:04:01 -0500

    [ReactUI] - To receive higher file sizes

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ import mongoose from 'mongoose';
 
 const initialize = (app) => {
     app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded());
+    app.use(express.json({limit: '50mb'}));
+    app.use(express.urlencoded({limit: '50mb'}));
     // mongoose.connect('mongodb+srv://vijayachandran-a:2DyQTUKnpp2AmvEG@info6150fall2023.h1f7hev.mongodb.net/?retryWrites=true&w=majority');
     //mongoose.connect('mongodb+srv://thomsonth:ffs2sjXiAQIvnqmP@info6150-test.twitsfc.mongodb.net/coursedb?retryWrites=true&w=majority');
     const url = 'mongodb://127.0.0.1/locall';
