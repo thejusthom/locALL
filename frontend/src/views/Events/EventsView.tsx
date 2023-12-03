ff6182748b61cf2b347aec9cc9a1de5133094785
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-03 03:36:36 -0500

    cleanup

--- frontend/src/views/Events/EventsView.tsx
+++ frontend/src/views/Events/EventsView.tsx
@@ -276,7 +276,7 @@ function a11yProps(index: number) {
   const FormFieldsComponent = () => {return(
     <>
     <InputWrap>
-    <label>Name:</label>
+    <label><MandatoryStar>*</MandatoryStar>Name:</label>
     <input type="text" id="eventName" value={newEvent.eventName} onChange={onNameChange} />
     </InputWrap>
     <InputWrap>
@@ -421,72 +421,9 @@ height: 100%;
 background-color: rgba(0,0,0,0.3);
 `;
 
-const Form = styled.form`
-background-color: #eceaea;
-width: 700px;
-height: 75%;
-place-self: center;
-/* align-self: center; */
-margin-top: 70px;
-border-radius: 5px;
-/* padding: 20px; */
-margin: 50px 0;
-color: #171717;
-overflow-y: auto;
-label{
-    font-size: 18px;
-    width: 200px;
-    text-align: left;
-}
-input, textarea, select{
-    border: none;
-    width: 440px;
-    height: 35px;
-    border-radius: 5px;
-}
-input:focus, textarea:focus{
-    outline: 0;
-}
-textarea{
-    height: 100px;
-}
-button{
-    margin: 20px 0;
-}
-`;
-const Heading = styled.section`
-/* margin: 0 0 30px 0; */
-background-color: #1976d2;
-position: fixed;
-width: 700px;
-border-radius: 5px 5px 0 0;
-display: flex;
-justify-content: space-between;
-padding: 20px;
-align-items: center;
-z-index: 10;
-img{
-    cursor: pointer;
-}
-h1{
-    margin: 0;
-    color: white;
-    font-weight: normal;
-    font-size: 25px;
-}`;
-const Content = styled.section`
-height: 100%;
-padding: 20px;
-margin-top: 83px;
-`;
 const InputWrap = styled.div`
     margin-bottom: 20px;
     display: flex;
-`;
-const FormWrap = styled.section`
-height: 100%;
-justify-content: center;
-display: flex;
 `;
 const Button = styled.button`
 background-color: #1976d2;
@@ -497,6 +434,9 @@ border: none;
 border-radius: 25px;
 font-size: 16px;
 padding: 10px 25px;
+`;
+const MandatoryStar = styled.span`
+
 `;
 
 export default EventsView;
