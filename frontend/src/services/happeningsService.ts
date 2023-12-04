04a8aa41212c3cea738419f01d863edbc2a2bde4
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-04 04:02:13 -0500

    file type change

---
+++ frontend/src/services/happeningsService.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import Happenings from "../models/happenings";
+
+const baseURL = "http://localhost:3001/location/";
+
+const getAllHappenings = async (locationId: string) => {
+  const response = await axios.get(baseURL + locationId + '/happenings');
+  return response.data;
+}
+
+const getHappeningById = async (locationId: string, happeningId: string) => {
+  const response = await axios.get(baseURL + locationId + '/happenings/' + happeningId);
+  return response.data;
+}
+
+const createHappening = async (locationId: string, newHappening: Happenings) => {
+  const response = await axios.post(baseURL + locationId + '/happenings', newHappening);
+  return response.data;
+};
+
+const updateHappening = async (locationId: string, happeningId: string, newHappening: Happenings) => {
+  const response = await axios.put(baseURL + locationId + '/happenings/' + happeningId, newHappening);
+  return response.data;
+}
+
+const deleteHappening = async (locationId: string, happeningId: string) => {
+  const response = await axios.delete(baseURL + locationId + '/happenings/' + happeningId);
+  return response.data;
+}
+
+const happeningsService = { getAllHappenings, getHappeningById, createHappening, updateHappening, deleteHappening };
+export default happeningsService;
