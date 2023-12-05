4054ffcd7eaef26763a1cc10e94878cdd61271f6
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2023-12-05 17:21:19 -0500

    Enable create user(POST) in register page

--- frontend/src/views/RegisterPage/register.tsx
+++ frontend/src/views/RegisterPage/register.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import { IUser, IPerson} from '../../models/user';
+import { useSelector } from 'react-redux';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography, {TypographyProps} from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import userService from '../../services/userService';
 
 const Copyright = (props: TypographyProps) => {
   return (
@@ -26,19 +27,50 @@ const Copyright = (props: TypographyProps) => {
   );
 }
 
+const initialStateUser = {
+  person: {} as IPerson,
+  username: '',
+  password: ''
+};
+
 const Register: React.FC = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //const locationId = useSelector((state: any) => state.location.pincode);
+  const [newUser, setNewUser] = useState<IUser>(initialStateUser);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      username: data.get('userName'),
-      password: data.get('password'),
-      firstName: data.get('firstName'),
-      lastName: data.get('lastName'),
-      address: data.get('address'),
-      zipCode: data.get('zipCode')
+
+    setNewUser({
+      ...newUser,
+      person: {
+        firstName: String(data.get('firstName')),
+        lastName: String(data.get('lastName')),
+        address: String(data.get('address')),
+        zipcode: String(data.get('zipCode')),
+        phoneNumber: String(data.get('phoneNo')),
+      },
+      username: String(data.get('userName')),
+      password: String(data.get('password')),
     });
+
+    try
+    {
+      const createdUser = await userService.createUser(newUser);
+      console.log('User created:', createdUser);
+      window.location.replace('/');
+    } 
+    catch(error)
+    {
+      console.error(`Error creating user: ${error}`);
+    }
+
   };
+
+  // Log the values when the state is updated
+  useEffect(() => {
+    console.log(newUser);
+  }, [newUser]);
 
   return (
     <ThemeProvider theme={createTheme()}>
@@ -85,10 +117,10 @@ const Register: React.FC = () => {
               <TextField
                 required
                 fullWidth
-                name="PhoneNo"
+                name="phoneNo"
                 label="PhoneNo"
                 type="PhoneNo"
-                id="PhoneNo"
+                id="phoneNo"
                 autoComplete="phoneNo"
               />
             </Grid>
