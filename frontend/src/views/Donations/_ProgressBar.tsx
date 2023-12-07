f5b01dfc18b2fa297d63ddadcc744b960b15f0bf
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-07 00:25:17 -0500

    [ReactUI] progress bar css

--- frontend/src/views/Donations/_ProgressBar.tsx
+++ frontend/src/views/Donations/_ProgressBar.tsx
@@ -18,6 +18,26 @@ const Progress_bar = (props: IProgressBar) => {
 	)
 }
 
+const Parentdiv = styled.div<{height: number}>`
+    height: ${(props) => props.height};
+    width: '100%';
+    background-color: 'whitesmoke';
+    border-radius: 40;
+    margin: 50;
+    `;
 
+const Childdiv = styled.div<{progress: number, bgColor: string}>`
+    height: '100%';
+    width: ${(props) => props.progress}%;
+    background-color:  ${(props) => props.bgColor};
+border-radius:40;
+    text-align: 'right';
+`;
+
+const ProgressText = styled.span`
+padding: 10;
+    color: 'black';
+    font-weight: 900;
+    `;
 
 export default Progress_bar;
