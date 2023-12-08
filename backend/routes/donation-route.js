fdfd88f8719fb74608e7b4bd990c4419abda5cb0
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-08 01:47:43 -0500

    [ReactBE] donation route

--- backend/routes/donation-route.js
+++ backend/routes/donation-route.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import * as donationController from '../controllers/donation-controller.js';
-import stripe from 'stripe';
-
-const stripeInstance = stripe(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 const donationRouter = express.Router();
 
@@ -16,33 +13,5 @@ donationRouter.route('/:donationId')
     .get(donationController.getById)
     .put(donationController.update)
     .delete(donationController.remove);
-
-    donationRouter.route("/payment/create-checkout-session").post(async(req,res)=>{
-        const {products} = req.body;
-    
-    
-        const lineItems = products.map((product)=>({
-            price_data:{
-                currency:"usd",
-                product_data:{
-                    name:product.name,
-                    // images:[product.imgdata]
-                },
-                unit_amount:product.price * 100,
-            },
-            quantity:1,
-        }));
-    
-        const session = await stripeInstance.checkout.sessions.create({
-            payment_method_types:["card"],
-            line_items:lineItems,
-            mode:"payment",
-            success_url:"http://localhost:3000/donations/sucess",
-            cancel_url:"http://localhost:3000/donations/cancel",
-        });
-    
-        res.json({id:session.id})
-     
-    })
 
 export default donationRouter;
