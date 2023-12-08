d39a00dd113c0b74c438b6f0a5d1d47c8b98862b
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-08 02:19:10 -0500

    [ReactInt] donations integration

--- frontend/src/views/Donations/DonationsView.tsx
+++ frontend/src/views/Donations/DonationsView.tsx
@@ -11,6 +11,7 @@ import { useSelector } from 'react-redux';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { ToastContainer, toast } from "react-toastify";
+import MyDonations from "./_MyDonations";
 
 const donationsList = [1, 2, 3,4];
 
@@ -32,8 +33,8 @@ const DonationsView = () => {
     const [newDonation, setNewDonation] = React.useState<IDonation>(initialDonationState);
     const [isValid, setIsValid] = React.useState(false);
     const [tab, setTab] = React.useState(0);
+    const [donationId, setDonationId] = React.useState("");
     const [isEdit, setIsEdit] = React.useState<boolean>(false);
-    const [eventId, setEventId] = React.useState<string>("");
 
     const selectLocation = (state: any) => state.location;
 const loc = useSelector(selectLocation);
@@ -107,8 +108,36 @@ React.useEffect(() => {
         setNewDonation({...newDonation, amountRequired: parseInt(e.target.value)});
     }
     const onUpdate = () => {
-
+        const updatedEvent = {...newDonation};
+        donationServices.updateDonation(loc.pincode, donationId, updatedEvent).then((donation)=> {
+            donationServices.getDonations(loc.pincode).then((donation)=> {
+                // const availableEvents = event.filter((e: IEvent) => !!e.endDate && moment(e.endDate) >= moment());
+                setDonations(donation)}
+        );
+        toast.success(`${donation.donationName} Updated Successfully!`);
+    });
+            setNewDonation(initialDonationState);
+            setIsEdit(false);
+            setDonationId("");
+            setShowModal(false);
     };
+    const onEdit = (donationId: string) => {
+        donationServices.getDonationById(loc.pincode, donationId).then((donation)=> {
+           setNewDonation(donation);
+           setIsEdit(true);
+           setDonationId(donationId);
+           setShowModal(true);
+       });
+   };
+   const onDelete = (donationId: string) => {
+       donationServices.deleteDonation(loc.pincode, donationId).then((donation)=> {
+           donationServices.getDonations(loc.pincode).then((donation)=> {
+            //    const availableEvents = donation.filter((e: IDonation) => !!e.endDate && moment(e.endDate) >= moment());
+               setDonations(donation)
+           });
+               toast.success(`Donation Deleted Successfully!`);
+       });
+   };
     const onSubmit = (event: any) => {
         event.preventDefault();
         donationServices.createDonation(loc.pincode, {...newDonation, createdUser: "656bbf4a3b7690ac27e2bcfb", locationId: loc.pincode, postedOn: new Date().toLocaleDateString()}).then((donation)=> {
@@ -153,9 +182,12 @@ React.useEffect(() => {
           <Tab sx={{fontSize: "16px", fontWeight: "bold"}} label="All Donations" {...a11yProps(0)} />
           <Tab sx={{fontSize: "16px", fontWeight: "bold"}} label="My Donations" {...a11yProps(1)} />
         </Tabs>
+        {tab === 0 && !!donations ? 
         <DonationCardsWrap>
         {donationsList.map(() => {return(<DonationCard handleMakePayment={handleMakePayment} />)} )}
-        </DonationCardsWrap>
+        </DonationCardsWrap> 
+        : <MyDonations donations={donations} onEdit={onEdit} onDelete={onDelete} />
+        }
         </>
     );
 }
