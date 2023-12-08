7b15d144c738cf50f4a383ce27f5c812fb4ee2f3
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-07 23:47:57 -0500

    [ReactFE] donations view

--- frontend/src/views/Donations/DonationsView.tsx
+++ frontend/src/views/Donations/DonationsView.tsx
@@ -1,14 +1,58 @@
 import * as React from "react";
 import styled from "styled-components";
 import {loadStripe} from '@stripe/stripe-js';
-import CheckoutForm from "./SubscriptionPlans";
+// import CheckoutForm from "./SubscriptionPlans";
 // import Girl from "../../assets/images/girl1.jpg";
-import { Button } from "../Events/_EventsForm";
+import donationServices from "../../services/donationsService";
+import EventsForm, { Button } from "../Events/_EventsForm";
 import DonationCard from "./_DonationCard";
+import { Modal } from "../Events/EventsView";
+import { IDonation } from "../../models/donation";
+import DonationForm from "./_DonationForm";
+import { useSelector } from 'react-redux';
+import { ToastContainer, toast } from "react-toastify";
 
-const donations = [1, 2, 3,4];
+const donationsList = [1, 2, 3,4];
+
+const initialDonationState = {
+    donationName: "",
+      postedOn: "",
+      descriptionInfo: "",
+      amountRequired: 0,
+      amountAchieved: 0,
+      createdUser: "",
+      locationId: "",
+      category: "medical",
+      receiver: {
+        name: "",
+        age: 0,
+        contact: "",
+        }
+}
 
 const DonationsView = () => {
+    const [donations, setDonations] = React.useState<IDonation[]>();
+    const [showModal, setShowModal] = React.useState(false);
+    const [newDonation, setNewDonation] = React.useState<IDonation>(initialDonationState);
+    const [isValid, setIsValid] = React.useState(false);
+
+    const selectLocation = (state: any) => state.location;
+const loc = useSelector(selectLocation);
+
+React.useEffect(() => {
+    const newDonationValues = Object.values(newDonation);
+    const receiverValues = Object.values(newDonation.receiver);
+    // const {eventName, descriptionInfo, category} = newEvent;
+    // const eventValues = [eventName, descriptionInfo, category];
+    if(newDonationValues.includes("") || newDonationValues.includes(0) || receiverValues.includes("") || receiverValues.includes(0)){
+        setIsValid(false);
+    }
+    else{
+        setIsValid(true);
+    }
+    }, [newDonation]);
+
+    // const []
     const handleMakePayment = async()=>{
         const stripe = await loadStripe("pk_test_51OKDhEAul8oR9y69J7pdRY2qMBmY6sux6srL1tbqBlmoaKW0OsuutywjQATMAadyiX60Wmtp0CSxYT2QsHuGVSn1004nwatlKT");
   
@@ -34,9 +78,73 @@ const DonationsView = () => {
             console.log(result.error);
         }
     }
+        const onCloseModal = () => {
+        // setNewEvent(initialNewEvent);
+        // setCoordinates({longitude: 0, latitude:0});
+        // setStartDate(undefined);
+        // setEndDate(undefined);
+        // setOrganiser({name: "", contact: ""});
+        // if(isEdit){
+        //     setIsEdit(false);
+        //     setEventId(""); 
+        // }
+        // setShowModal(false);
+    };
+    const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewDonation({...newDonation, donationName: e.target.value});
+    }
+    const onDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewDonation({...newDonation, descriptionInfo: e.target.value});
+    } 
+    const onCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setNewDonation({...newDonation, category: e.target.value});
+    }
+    const onReceiverChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewDonation({...newDonation, receiver: {...newDonation.receiver, [e.target.id]: e.target.value}});
+    }
+    const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewDonation({...newDonation, amountRequired: parseInt(e.target.value)});
+    }
+    const onUpdate = () => {
+
+    };
+    const onSubmit = (event: any) => {
+        event.preventDefault();
+        donationServices.createDonation(loc.pincode, {...newDonation, createdUser: "656bbf4a3b7690ac27e2bcfb"}).then((donation)=> {
+            !!donations ? setDonations([...donations, donation]) : setDonations([donation]);
+            toast.success("Donation Created Successfully!");
+        });
+        setShowModal(false);
+        setNewDonation(initialDonationState);
+    };
     return(
         <>
-        {donations.map(() => {return(<DonationCard handleMakePayment={handleMakePayment} />)} )}
+        <Modal isOpen={showModal}>
+  <EventsForm 
+  isEdit={false}
+   eventName={newDonation.donationName}
+   onCloseModal={onCloseModal} 
+   onUpdate={onUpdate} 
+   onSubmit={onSubmit}
+   isDisabled={!isValid}
+   children={<DonationForm
+              newDonation={newDonation}
+              onCategoryChange={onCategoryChange}
+              onNameChange={onNameChange}
+              onDescriptionChange={onDescriptionChange}
+              onReceiverChange={onReceiverChange}
+              onAmountChange={onAmountChange}
+            //   onStartDateChange={onStartDateChange}
+            //   onEndDateChange={onEndDateChange}
+            //   onLocationChange={onLocationChange} 
+            //   onOrganiserChange={onOrganiserChange}
+              isEdit={false}
+            />
+            }
+    />
+  </Modal>
+        <Button onClick={() => setShowModal(true)}>Create New Donation</Button>
+        {donationsList.map(() => {return(<DonationCard handleMakePayment={handleMakePayment} />)} )}
         {/* <Card>
             <img src={Girl} />
             <div>
@@ -50,7 +158,7 @@ const DonationsView = () => {
             <Button>Donate Now</Button>
             </div>
         </Card> */}
-        <CheckoutForm />
+        {/* <CheckoutForm /> */}
         </>
     );
 }
