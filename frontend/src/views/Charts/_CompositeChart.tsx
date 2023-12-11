63f6a7ea7b4ccc4421f57310b96ce33a58655e46
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-11 15:35:09 -0500

    custom tooltip in composite chart

--- frontend/src/views/Charts/_CompositeChart.tsx
+++ frontend/src/views/Charts/_CompositeChart.tsx
@@ -21,7 +21,28 @@ line2Key: string;
 
 const CompositeChartComp = (props: ICompositeChartProps) => {
     const { compositeChartData, dataKey, areaKey, barKey, line1Key, line2Key } = props;
-
+    const CustomTooltip = ({ active, payload, label }: any) => {
+        console.log(payload, label)
+        if (active && payload && payload.length) {
+            const Circle = (props: {fill: string}) => {
+    return(<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" width="15" height="15">
+    <circle cx="7.5" cy="7.5" r="3" fill={props.fill} />
+    </svg>);
+            }
+          return (
+            <div className="tooltip-wrap">
+              <p className="label">{label}</p>
+              {/* <p className="intro">{getIntroOfPage(label)}</p> */}
+              <p><span><Circle fill="#123abc" /></span>Amount Required: {payload[0].payload.amountRequired}</p>
+              <p><span><Circle fill="#1976d2" /></span>Amount Achieved: {payload[0].payload.amountAchieved}</p>
+              <p><span><Circle fill="#38d200" /></span>Below 50 Age: {payload[0].payload.below50}</p>
+              <p><span><Circle fill="violet" /></span>Above 50 Age: {payload[0].payload.above50}</p>
+            </div>
+          );
+        }
+        else return null;
+    }
+    
     return(
         <ComposedChart
         width={1000}
