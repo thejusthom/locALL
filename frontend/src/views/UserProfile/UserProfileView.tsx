c98d59e427dcbac479e21672d9d30f911776a3f4
Ashmiya VijayaChandran <vijayachandran.a@northeastern.edu>
2023-12-12 19:41:58 -0500

    user profile changes

--- frontend/src/views/UserProfile/UserProfileView.tsx
+++ frontend/src/views/UserProfile/UserProfileView.tsx
@@ -1,30 +1,43 @@
 import * as React from 'react';
+import styled from "styled-components";
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
+// import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
+// import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import UserProfileBlock from './UserProfileBlock';
 
 export default function UserProfileView() {
-  const [value, setValue] = React.useState('1');
+  // const [value, setValue] = React.useState('1');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+  //   setValue(newValue);
+  // };
 
   return (
     <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange}>
+      {/* <TabContext value={value}> */}
+        {/* <Box sx={{ borderBottom: 1, borderColor: 'divider' }}> */}
+          {/* <TabList onChange={handleChange}>
             <Tab label="Profile" value="1" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">
-          <UserProfileBlock></UserProfileBlock>
-        </TabPanel>
-      </TabContext>
+          </TabList> */}
+        {/* </Box> */}
+        {/* <TabPanel value="1"> */}
+       <ProfileWrap>
+         <h2>Profile</h2>
+          <UserProfileBlock />
+          </ProfileWrap>
+        {/* </TabPanel> */}
+      {/* </TabContext> */}
     </Box>
   );
 }
+
+const ProfileWrap = styled.article`
+margin: 20px;
+h2{
+  margin: 0 0 0 10px;
+    font-size: 28px;
+    color: #1976d2;
+}
+`;
